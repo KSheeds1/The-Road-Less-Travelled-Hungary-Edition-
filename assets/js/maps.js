e7ae01f193a79d98af82cc823c2e59c77323a0eb
KSheeds1 <katiesheedy@hotmail.com>
2020-11-07 17:18:58 +0000

    Refactor code for the google maps API to allow for additional maps to be added cross-site. Modify the markers function creating a markerLocations array and use a for loop to call and create the markers, initialise the maps for budapest.html and keszthely.html in preparation for integrating the places API.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,12 @@
+function initMaps() {
+    initMap();
+    initBudapestMap();
+    initKeszthelyMap();
+    
+}
+
 function initMap() {
-    let map = new google.maps.Map(document.getElementById("map"), {
+    let map = new google.maps.Map(document.getElementById("mapDestinations"), {
         zoom: 7,
         center: { 
             lat: 47.497913, 
@@ -10,44 +17,71 @@ function initMap() {
     let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     let locations = [
-        { name:"Budapest", lat: 47.496717,  lng: 19.013397, url: "budapest.html"},
-        { name: "Pécs", lat: 46.0751089, lng: 18.2261525, url: "pecs.html"},
-        { name: "Siofok", lat: 46.9019145, lng: 18.0447842, url: "siofok.html" },
-        { name: "Keszthely", lat: 46.7498531, lng: 17.1719147, url: "keszthely.html"}
+        {  lat: 47.496717,  lng: 19.013397 },
+        {  lat: 46.0751089, lng: 18.2261525 },
+        {  lat: 46.9019145, lng: 18.0447842 },
+        {  lat: 46.7498531, lng: 17.1719147 }
     ];
     
-    let markers = locations.map(function(location, i) {
+    let markerLocations = [
+        { name: "Budapest", lat: 47.496717,  lng: 19.013397, url: "budapest.html" },
+        { name: "Pécs", lat: 46.0751089, lng: 18.2261525, url: "pecs.html" },
+        { name: "Siofok", lat: 46.9019145, lng: 18.0447842, url: "siofok.html" },
+        { name: "Keszthely", lat: 46.7498531, lng: 17.1719147, url: "keszthely.html" }
+    ];
+  
+   
+    for (let i = 0; i < markerLocations.length; i++) {
         let marker = new google.maps.Marker({
+            position: { lat: markerLocations[i].lat, lng: markerLocations[i].lng},
             map,
+            animation: google.maps.Animation.DROP,
+            title: "",
+            url: markerLocations[i].url,
+            label: labels[i % labels.length]
+        });
+
+        google.maps.event.addListener(marker, "click", function() {
+            window.location.href = marker.url;
+        });
+    }
+    
+
+    
+
+    /*let markers = locations.map(function(location, i) {
+        let marker = new google.maps.Marker({
+            mapDestinations,
             draggable: false,
             animation: google.maps.Animation.DROP,
             position: location,
             url: locations[i].url,
             label: labels[i % labels.length]
         });
+        
         google.maps.event.addListener(marker, "click", function() {
             window.location.href = this.url;
         });
-    });
-
+    });*/
+  
 
 }
 
-function initMap() {
-    let map = new google.maps.Map(document.getElementById("mapBudapest"), {
+function initBudapestMap() {
+    let mapBudapest = new google.maps.Map(document.getElementById("mapBudapest"), {
         zoom: 12,
         center: { 
-            lat: 47.496717,  
+            lat: 47.496717,
             lng: 19.013397
         }
     });
 }
 
-function initMap() {
-    let map = new google.maps.Map(document.getElementById("mapKeszthely"), {
+function initKeszthelyMap() {  
+    let mapKeszthely = new google.maps.Map(document.getElementById("mapKeszthely"), {
         zoom: 12,
         center: { 
-            lat: 46.7498531, 
+            lat: 46.7498531,
             lng: 17.1719147
         }
     });
